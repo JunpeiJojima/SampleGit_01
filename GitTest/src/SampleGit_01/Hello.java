e37499f37f196936357ec11babc9dc05659ab390
Joji1234 <degarasiprograming@gmail.com>
2021-06-27 14:15:06 +0900

    Helloクラス作成、mainでのHello呼び出しの追加

---
+++ GitTest/src/SampleGit_01/Hello.java
@@ -0,0 +1,9 @@
+package SampleGit_01;
+
+public class Hello {
+	
+	public void sysHello() {
+		System.out.println("Hello!");
+	}
+
+}
--- GitTest/src/SampleGit_01/Main.java
+++ GitTest/src/SampleGit_01/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		Hello hl = new Hello();
+		hl.sysHello();
 		System.out.println("Git Test dayo!");
 		
 	}
