0d9f9f647d6340e252120110b634624d3bb08e3f
Joji1234 <degarasiprograming@gmail.com>
2021-06-27 14:52:17 +0900

    Goonightクラスの追加,mainメソッドからの実装

--- GitTest/src/SampleGit_01/Main.java
+++ GitTest/src/SampleGit_01/Main.java
@@ -4,8 +4,20 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		Hellosys();
 		System.out.println("Git Test dayo!");
-		
+		Goonight gn = new Goonight();
+		gn.gooNight();
+	}
+	private static void Hellosys() {
+		System.out.println("Hello～.");
 	}
 
+}
+class Goonight{
+	
+	void gooNight() {
+	System.out.println("goonight!");
+
+	}
 }
